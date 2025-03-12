a2da795b037d99d8004f18b8f98fb68c360789c3
Nightdragoon <crackytrini@gmail.com>
2025-03-12 00:19:50 -0600

    fdfd
    fdf

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express()
 var mysql = require("mysql")
-var cors = require("cors")
+var cors = require("cors");
+
 
 var conn = mysql.createConnection({
     host:"b6jjaxkzutbd4pfc90tk-mysql.services.clever-cloud.com",
@@ -14,38 +15,46 @@ var conn = mysql.createConnection({
 
 
 app.use(cors())
-app.get("/api" , (req,res) => {
-    res.json({"hola":"pena", "gordos":"locos"})
-})
 
 app.get("/consulta",(req,res) => {
     var no = req.query.no;
     var results;
     var dinero;
+    const start = process.hrtime(); // Iniciar medición de tiempo
+
     conn.connect(function(err) {
+
         conn.query("SELECT dinero FROM Clientes WHERE noCuenta = " + no + ";", function(err,result,fields){
             results = JSON.stringify(result);
-            console.log(results)
+
+            console.log(results);
             var json =  JSON.parse(results);
+            const diff = process.hrtime(start); // Terminar medición
+            const tiempo = (diff[0] * 1e3 + diff[1] / 1e6).toFixed(3); // Convertir a milisegundos
+    
+            console.log(`Tiempo SQL consulta: ${tiempo}ms`); // Mostrar en consola
             res.json({"dinero":json[0].dinero})
 
         });
 
 
     });
+   
+
     
 })
 app.get("/eliminar",(req,res) =>{
+    var a = performance.now();
     var no = req.query.no;
     console.log("elimnando el " + no )
     conn.connect(function(err) {
         console.log("conectado")
-        if (err) throw err;
         var sql = "DELETE FROM Clientes WHERE noCuenta = " + no + ";"
         conn.query(sql,function(err,result){
             if (err) throw err;
             console.log("Number of records deleted: " + result.affectedRows);
-            res.json({"status":"eliminado"})
+            var b = performance.now();
+            res.json({"status":"eliminado" , "performance": (b-a)})
 
 
         })
